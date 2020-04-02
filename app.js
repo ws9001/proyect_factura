83549709b44280e91e470e756770ecf26823be8d
ws9001 <53454747+ws9001@users.noreply.github.com>
2020-04-02 15:34:12 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+let fact_numero;
+let fch_emision = new Date();
+let ven_cond;
+let cli_rsocial;
+let cli_ruc;
+let cli_direccion;
+let cli_telefono;
+let cli_nremision;
+let item_cod;
+let item_cant;
+let item_descrip;
+let precio_unit;
+let iva_diez;
+let iva_cinco;
+let iva_exe;
+let total_parcial;
+let total_letras;
+let iva_liqdiez;
+let iva_liqcinco;
+let iva_liqexe;
+
+
+fact_numero = prompt("Ingrese numero de factura");
+
+alert(fact_numero)
+fch_emision = prompt("Ingrese fecha");
+ven_cond = prompt ("Si es al CONTADO presione 1 CREDITO 2")
+// if (vent_cond===1){
+// 	alert("es Contado");
+// }else{
+// alert("Es credito")
+// }
+cli_rsocial = prompt("Ingrese la Razon Social");
+cli_ruc = prompt("Ingrese numero de R.U.C.");
+cli_direccion = prompt("Ingrese direccion del cliente");
+cli_telefono = prompt("Ingrese nro de telefono del cliente");
+cli_nremision = prompt("Ingrese nota de remision");
+item_cod = prompt("Ingrese codigo de item");
+item_cant = prompt("Ingrese cantidad de item"); 
+item_descrip = prompt("Ingrese descripcion del item");
+precio_unit = prompt("Ingrese precio unitario");
+// let iva_diez;
+// let iva_cinco;
+// let iva_exe;
+// let total_parcial;
+// let total_letras;
+// let iva_liqdiez;
+// let iva_liqcinco;
+// let iva_liqexe;
